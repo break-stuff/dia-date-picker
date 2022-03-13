bb7fef7b3ba7f8f94f41319efced833c19816986
Burton Smith <burton@breakstuff.io>
2022-03-13 11:29:36 -0400

    include shadowRoot in query

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -1,11 +1,11 @@
-const FOCUSABLE_ELEMENTS = [
+export const FOCUSABLE_ELEMENTS = [
   'input:not([disabled])',
   'select:not([disabled])',
   'button:not([disabled]):not([tabindex="-1"])',
   '[tabindex]:not([tabindex="-1"])',
 ].join(',');
 
-export function getFocusableElements($element: HTMLElement | undefined) {
+export function getFocusableElements($element: HTMLElement | ShadowRoot | null | undefined) {
   return Array.prototype.slice.call(
     $element?.querySelectorAll(FOCUSABLE_ELEMENTS)
   );
