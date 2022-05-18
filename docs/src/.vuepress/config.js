22600ada0e8d2d7e1037d06748d924d170f9b709
Burton Smith <burton@breakstuff.io>
2022-05-18 15:16:45 -0400

    add static assets

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -16,6 +16,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
+    ['link', { rel: 'icon', href: '/favicon.svg' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     [
